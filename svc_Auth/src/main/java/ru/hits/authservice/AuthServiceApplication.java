af8554d3a40d9c459dfacae8a440c0b66555e966
iseeyouwatching <leonidshibanovv@gmail.com>
2024-05-01 20:55:51 +0700

    Инициализация проекта

---
+++ svc_Auth/src/main/java/ru/hits/authservice/AuthServiceApplication.java
@@ -0,0 +1,15 @@
+package ru.hits.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan("ru.hits.authservice")
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
