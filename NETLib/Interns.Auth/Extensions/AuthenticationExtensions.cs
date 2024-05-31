578b0802a2852b886bbd894b513b5a69630b3c1d
Volgin Ilia <govnihorse@gmail.com>
2024-05-31 19:10:40 +0700

    feat: add pkg w/ auth stuff

---
+++ NETLib/Interns.Auth/Extensions/AuthenticationExtensions.cs
@@ -0,0 +1,82 @@
+﻿using Interns.Auth.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Interns.Auth.Extensions
+{
+    public static class AuthenticationExtensions
+    {
+        /// <summary>
+        /// Overload for default ConfigureAuth that requires "Auth" section in appsettings.json or any configuration provider
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static WebApplicationBuilder ConfigureAuth(this WebApplicationBuilder builder)
+        {
+            var authOptions = builder.Configuration.GetSection("Auth").Get<AuthOptions>();
+
+            ArgumentNullException.ThrowIfNull(authOptions);
+            ArgumentException.ThrowIfNullOrEmpty(authOptions?.Key);
+            ArgumentException.ThrowIfNullOrEmpty(authOptions?.Issuer);
+
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(authOptions.Key));
+
+            return builder.ConfigureAuth(authOptions.Issuer, securityKey);
+        }
+
+        /// <summary>
+        /// Default method for configuration auth, adds jwt handling and stuff
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="issuer"></param>
+        /// <param name="issuerSigningKey"></param>
+        public static WebApplicationBuilder ConfigureAuth(
+            this WebApplicationBuilder builder,
+            string issuer,
+            SecurityKey issuerSigningKey
+        )
+        {
+            builder.Services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.Authority = issuer;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuers = [issuer],
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        IssuerSigningKey = issuerSigningKey,
+                        ValidateIssuerSigningKey = true
+                    };
+
+                    // leave for [Authorize] debugging: set breakpoint on lambda
+#pragma warning disable
+                    options.Events = new()
+                    {
+                        OnAuthenticationFailed = async ctx =>
+                        {
+                            var breakpoint = true;
+                        }
+                    };
+#pragma warning restore
+                });
+
+            builder.Services.AddAuthorization();
+
+            return builder;
+        }
+    }
+}
---
+++ NETLib/Interns.Auth/Options/AuthOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Interns.Auth.Options
+{
+    public class AuthOptions
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+    }
+}
