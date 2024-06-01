54f813140c642704294a9a13b4aed9ec606c6b23
Ilia Volgin <govnihorse@gmail.com>
2024-06-01 22:35:15 +0700

    ci: upd db shell script

--- database/init.sh
+++ database/init.sh
@@ -7,6 +7,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE "auth-service-db";
     CREATE DATABASE "company-management-service-db";
     CREATE DATABASE "interview-db";
+    CREATE DATABASE "interview-chat"
 EOSQL
 
 echo "Databases created successfully."
