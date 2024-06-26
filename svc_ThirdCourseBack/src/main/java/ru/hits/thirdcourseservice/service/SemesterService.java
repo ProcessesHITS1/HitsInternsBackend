1c6459aa7d4ddf4497071836ec0f28ab5fca539d
iseeyouwatching <leonidshibanovv@gmail.com>
2024-06-26 18:57:01 +0700

    Правки

--- svc_ThirdCourseBack/src/main/java/ru/hits/thirdcourseservice/service/SemesterService.java
+++ svc_ThirdCourseBack/src/main/java/ru/hits/thirdcourseservice/service/SemesterService.java
@@ -155,6 +155,7 @@ public class SemesterService {
                         .year(entity.getSemester().getYear())
                         .semester(entity.getSemester().getSemester())
                         .seasonId(entity.getSemester().getSeasonId())
+                        .documentsDeadline(entity.getSemester().getDocumentsDeadline())
                         .isClosed(entity.getSemester().getIsClosed())
                         .build())
                 .collect(Collectors.toList());
